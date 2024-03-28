d1cf141f6ca744637f65c8c1997e0676f2c139f9
Zachary Walden <zachary.walden@eagles.oc.edu>
2024-03-28 09:39:59 -0500

    Re add some reverted changes

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
 static char *font2[] = {
-	"SymbolsNerdFont-Regular:pixelsize=16:antialias=true:autohint=true",
+	"SymbolsNerdFont:pixelsize=16:antialias=true:autohint=true",
 	"NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"
 };
 static int borderpx = 2;
@@ -32,9 +32,6 @@ char *vtiden = "\033[?6c";
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
 static float chscale = 1.0;
-/* Character rendering offsets in pixels */
-static short cxoffset = 0;
-static short cyoffset = 0;
 
 /*
  * word delimiter string
